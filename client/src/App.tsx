a6ec68abd6c90f8d4284090571f7b6b3034fdae2
Amaechi747 <amaechimoses8@gmail.com>
2022-08-22 03:21:24 +0100

    Fixing nothing is displaying

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
