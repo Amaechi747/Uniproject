6bd5cea90b74254fbb7e3793c1eb7af8224fc0a7
Amaechi747 <amaechimoses8@gmail.com>
2022-08-22 03:00:55 +0100

    Working small small

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { about } from "../images"
+// import { about } from "../images"
 
 type Props = {}
 
@@ -17,7 +17,7 @@ const About = (props: Props) => {
         <div className="spacer">
           <div className="row">
             <div className="col-lg-8  col-lg-offset-2">
-              <img src={about} className="img-responsive thumbnail" alt="realestate" />
+              <img className="img-responsive thumbnail" alt="realestate" />
               <h3>Business Background</h3>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
               <h3>Company Profile</h3>
